55af75c9fc7231474263b5f62ebd84522d2044cc
Mabel Duran-Sanchez <65778105+MabelDS89@users.noreply.github.com>
2020-06-22 15:10:07 -0400

    Calculated total months in dataset, net total profit/loss, and average change in profit/loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,30 +1,45 @@
-#1st import the OS module
+#1st import the OS and CSV modules
 import os
-
-#2nd import module for reading CSV and set path for "budget_data" file
 import csv
-budget_datacsv = os.path.join("Resources", "budget_data.csv")
-    
-#Assign values to variables
-date = str((budget_data[0])
-profitloss = int((budget_data[1]))
+
+#Set path for "budget_data" file
+budget_data_path = os.path.join("Resources", "budget_data.csv")
 
 #Open the "budget_data" CSV file and specify delimiter and variable that holds content
-with open(budget_datacsv) as csvfile:
-    csvreader = csv.reader(budget_datacsv, delimiter=",")
+with open(budget_data_path) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
   
     #Skip the Header Row
-    next(csvreader, None)
+    next(csvreader)
+
+    total_months = 0
+    net_total = 0
+    previous = 0
+    change = 0
+    difference = []
 
     #Read through each row of data skipping the header
-    for row in csvreader:
+    for x in csvreader:
         
-        #Find the total # of months in "budget_data" file by using the len() function
-        total_months = int(len(date))
+        #Find the total # of months in dataset
+        total_months = total_months + 1
 
         #Find the net total amount of "Profit/Losses" over the entire period
-        net_total_profit_losses = int(sum(profitloss))
+        net_total = net_total + int(x[1])
 
-        #Print values for total_months and net_total_profit_losses
-        print(f'"Total Months: " + (total_months)')
-        print(f'"Total: " + (net_total_profit_losses)')
+        #Find the difference between rows in colum 2
+        change = int(x[1]) - previous
+        previous = int(x[1])
+        difference.append(change)
+    
+        #Calculate the # of differences from month to month
+        length = len(difference) - 1
+
+    average = round((sum(difference[1:])/length),2)
+
+    #Print values for total_months and net_total_profit_losses
+    print("Financial Analysis")
+    print("------------------")
+    print(f"Total Months: {total_months}")
+    print(f"Total: {net_total}")
+    print(f"Average Change: {average}")
