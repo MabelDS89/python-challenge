355a9047be9b1b0933097ad4f64f0a4a7c9d3453
Mabel Duran-Sanchez <65778105+MabelDS89@users.noreply.github.com>
2020-06-22 16:50:02 -0400

    Most up to date version of the PyBank HW.

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,11 +12,12 @@ with open(budget_data_path) as csvfile:
     #Skip the Header Row
     next(csvreader)
 
+    #Assign variables
     total_months = 0
     net_total = 0
     previous = 0
     change = 0
-    difference = []
+    difference_list = []
 
     #Read through each row of data skipping the header
     for x in csvreader:
@@ -27,19 +28,26 @@ with open(budget_data_path) as csvfile:
         #Find the net total amount of "Profit/Losses" over the entire period
         net_total = net_total + int(x[1])
 
-        #Find the difference between rows in colum 2
+        #Find the difference between rows in colum 2 and store values to the difference list
         change = int(x[1]) - previous
         previous = int(x[1])
-        difference.append(change)
+        difference_list.append(change)
     
         #Calculate the # of differences from month to month
-        length = len(difference) - 1
+        length = len(difference_list) - 1
 
-    average = round((sum(difference[1:])/length),2)
+    #Calculate the average change in profit/loss
+    average = round((sum(difference_list[1:])/length),2)
+
+    #Calculate the greatest increase and decrease in profits
+    maxincrease = int(max(difference_list))
+    minincrease = int(min(difference_list))
 
     #Print values for total_months and net_total_profit_losses
     print("Financial Analysis")
     print("------------------")
     print(f"Total Months: {total_months}")
-    print(f"Total: {net_total}")
-    print(f"Average Change: {average}")
+    print(f"Total: ${net_total}")
+    print(f"Average Change: ${average}")
+    print(f"Greatest Increase in Profits: ${maxincrease}")
+    print(f"Greatest Decrease in Profits: ${minincrease}")
